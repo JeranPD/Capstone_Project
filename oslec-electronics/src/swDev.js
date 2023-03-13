d5c5376cb7873e283138ecb47935a7691e60ff6a
Jeran <109207378+JeranPD@users.noreply.github.com>
2023-03-13 15:23:52 +0800

    Delete oslec-electronics directory

--- oslec-electronics/src/swDev.js
+++
@@ -1,6 +0,0 @@
-export default function swDev() {
-    let swUrl = `${process.env.PUBLIC_URL}/serviceworker.js`;
-    navigator.serviceWorker.register(swUrl).then((response) => {
-        console.log('response', response)
-    })
-}
